45093358d841c478c635a38e801815e1f45cd9ad
jongwonkim <jongwonkim@mymusictaste.com>
2017-07-11 13:15:23 +0900

    Merge remote-tracking branch 'origin/dev2' into development
    # Conflicts:
    #	src/app/lambda/search-concert.py

--- src/lex/lambda/ask-city.py
+++ src/lex/lambda/ask-city.py
@@ -72,8 +72,10 @@ def compose_retry_response(event):
 
 def check_city(event):
     check = requests.get(os.environ['BIT_CITY_SEARCH'].format(
-            event['currentIntent']['slots']['City'].strip(),
-        )).json()
+        "MyMusicMate",
+        event['currentIntent']['slots']['City'].strip(),
+        0
+    )).json()
     if 'errors' in check and 'Unknown Location' in check['errors']:
         return False
     else:
